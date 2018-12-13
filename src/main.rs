7ab437562a17e51ebc72b2c925698a0d0125d195
greg <gperlman27@gmail.com>
2018-12-12 22:28:41 -0500

    initial use of elm within a webview

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate web_view;
+use web_view::*;
+
 fn main() {
-    println!("Hello, world!");
+    web_view::builder()
+        .title("Rust / Elm - Counter App")
+        .content(Content::Html(include_str!("../client/index.html")))
+        .size(320, 480)
+        .resizable(false)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
 }
