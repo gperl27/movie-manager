ac43c6b6f27647b11d5476e12b48f6576f0d6579
Greg Perlman <gperlman27@gmail.com>
2018-12-14 11:27:51 -0500

    open folder from incoming ipc

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 extern crate web_view;
+use std::env;
 use web_view::*;
 
 fn main() {
     let webview = web_view::builder()
-        .title("Rust / Elm - Counter App")
+        .title("Movie Manager")
         .content(Content::Html(create_html()))
         .size(320, 480)
         .resizable(true)
         .debug(true)
         .user_data(())
         .invoke_handler(|webview, arg| {
-            let data = webview.user_data();
-            println!("{:?}", arg);
-            println!("{:?}", data);
+            // let data = webview.user_data();
+            // println!("{:?}", arg);
+            // println!("{:?}", data);
             match arg {
-                "openFolder" => {
-                    println!("hello from rust");
-                }
+                "openFolder" => match webview
+                    .dialog()
+                    .choose_directory("Please choose a folder...", "")?
+                {
+                    Some(path) => println!("Folder at {}", path.to_string_lossy()),
+                    None => println!("Cancelled opening folder"),
+                },
                 _ => {
                     println!("got an ipc but doesnt match");
                 }
