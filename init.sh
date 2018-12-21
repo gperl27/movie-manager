d7564063181e2ab4640330495c2cf4634c1563b8
Greg Perlman <gperlman27@gmail.com>
2018-12-21 17:29:07 -0500

    attempt install script

---
+++ init.sh
@@ -0,0 +1,7 @@
+wget -qO- https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip | tar xvz -C src/client/vendor
+wget -qO- https://github.com/FortAwesome/Font-Awesome/releases/download/5.6.1/fontawesome-free-5.6.1-web.zip | tar xvz -C src/client/vendor
+
+rm -rf bulma-0.7.2.zip
+rm -rf fontawesome-free-5.6.1-web.zip
+
+cd src/client && ./optimize.sh src/Main.elm
