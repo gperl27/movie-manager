999df260ef3259b9e36ec9f800233b3c0b17b82a
Greg Perlman <gperlman27@gmail.com>
2018-12-24 07:33:49 -0800

    make init.sh script windows friendly

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-wget -qO- https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip | tar xvz -C src/client/vendor
-wget https://github.com/FortAwesome/Font-Awesome/releases/download/5.6.1/fontawesome-free-5.6.1-web.zip
+curl -LO https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip
+unzip bulma-0.7.2.zip -d src/client/vendor
 
+curl -LO https://github.com/FortAwesome/Font-Awesome/releases/download/5.6.1/fontawesome-free-5.6.1-web.zip
 unzip fontawesome-free-5.6.1-web.zip -d src/client/vendor
 
 rm -rf bulma-0.7.2.zip
