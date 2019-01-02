bc05b487bc904c4417d4af216c6ba2f0137f2654
Greg Perlman <gperlman27@gmail.com>
2019-01-02 16:17:08 -0500

    update init script

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 curl -LO https://github.com/jgthms/bulma/releases/download/0.7.2/bulma-0.7.2.zip
-unzip bulma-0.7.2.zip -d src/client/vendor
+unzip bulma-0.7.2.zip -d client/vendor
 
 curl -LO https://github.com/FortAwesome/Font-Awesome/releases/download/5.6.1/fontawesome-free-5.6.1-web.zip
-unzip fontawesome-free-5.6.1-web.zip -d src/client/vendor
+unzip fontawesome-free-5.6.1-web.zip -d client/vendor
 
 rm -rf bulma-0.7.2.zip
 rm -rf fontawesome-free-5.6.1-web.zip
 
-cd src/client && ./optimize.sh src/Main.elm
+cd client && ./optimize.sh src/Main.elm
